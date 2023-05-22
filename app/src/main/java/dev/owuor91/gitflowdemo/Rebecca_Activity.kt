c9a1e5c5486cbe43daf41a04e915b989ee8bc0e7
Rebeccanak <rebeccanakachwaaa@gmail.com>
2023-05-22 16:24:44 +0300

    added Rebecca_Activity

---
+++ app/src/main/java/dev/owuor91/gitflowdemo/Rebecca_Activity.kt
@@ -0,0 +1,11 @@
+package dev.owuor91.gitflowdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Rebecca_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rebecca)
+    }
+}
