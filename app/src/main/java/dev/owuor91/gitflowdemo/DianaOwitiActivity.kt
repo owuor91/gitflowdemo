1beb0ca64ce1a5b467240909187975234183ad7f
DianaAkinyi <dianaakinyiowiti@gmail.com>
2023-05-22 16:27:36 +0300

    DianaOwiti

---
+++ app/src/main/java/dev/owuor91/gitflowdemo/DianaOwitiActivity.kt
@@ -0,0 +1,11 @@
+package dev.owuor91.gitflowdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DianaOwitiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diana_owiti)
+    }
+}
