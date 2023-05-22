863143795931089b349ad2d109f5154b0baca16b
Marion <njeriwmarion@gmail.com>
2023-05-22 16:34:40 +0300

    added midevamarrion

---
+++ app/src/main/java/dev/owuor91/gitflowdemo/marrion_mideva.kt
@@ -0,0 +1,11 @@
+package dev.owuor91.gitflowdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class marrion_mideva : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_marrion_mideva)
+    }
+}
