3617675d3cacd0a5ac83b6f53603315ce1807f7b
Rose <rosekivuvan@gmail.com>
2023-05-22 16:23:47 +0300

    Added Rose feature

---
+++ app/src/main/java/dev/owuor91/gitflowdemo/RoseKivuvaActivity.kt
@@ -0,0 +1,11 @@
+package dev.owuor91.gitflowdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoseKivuvaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rose_kivuva)
+    }
+}
