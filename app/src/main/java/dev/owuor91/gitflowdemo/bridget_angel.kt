098a6b2ea80aeb6eed00d36def13be24eda22992
Nakakandebr <bridgetnakakandea@gmail.com>
2023-05-22 16:24:44 +0300

    added bridget_angel

---
+++ app/src/main/java/dev/owuor91/gitflowdemo/bridget_angel.kt
@@ -0,0 +1,11 @@
+package dev.owuor91.gitflowdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class bridget_angel : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bridget_angel)
+    }
+}
