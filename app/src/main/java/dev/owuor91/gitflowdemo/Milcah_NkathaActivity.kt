2e756f0e770c30b0685fda5e25d288c42d806adf
nkathamilcahmakini <nkathamilcah@gmail.com>
2023-05-22 16:25:12 +0300

    Added an activity Milcah_NkathaActivity

---
+++ app/src/main/java/dev/owuor91/gitflowdemo/Milcah_NkathaActivity.kt
@@ -0,0 +1,11 @@
+package dev.owuor91.gitflowdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Milcah_NkathaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_milcah_nkatha)
+    }
+}
